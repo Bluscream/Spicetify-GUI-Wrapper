2a16d8631f9e259c9a1684c592755338b5f4e501
Bluscream <admin@timo.de.vc>
2019-10-24 04:09:01 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CommandLine;
+
+namespace Spicetify_GUI_Wrapper
+{
+    internal static class Program
+    {
+        public static Options Arguments = new Options();
+
+        public class Options
+        {
+            [Option('c', "console", Required = false, HelpText = "Enable console")]
+            public bool ConsoleEnabled { get; set; }
+
+            [Option('q', "quiet", Required = false, HelpText = "Quiet mode (no output). Be careful, dangerous operations like clear backup, restore will proceed without prompting permission.")]
+            public string Quiet { get; set; }
+
+            [Option('e', "extension", Required = false, HelpText = "Use with \"update\", \"watch\" or \"path\" command to focus on extensions.")]
+            public bool Extension { get; set; } = false;
+
+            [Option('a', "app", Required = false, HelpText = "Use with \"path\" to focus on custom apps.")]
+            public bool App { get; set; } = false;
+
+            [Option('c', "config", Required = false, HelpText = "Print config file path and quit")]
+            public bool Config { get; set; } = false;
+
+            [Option('h', "help", Required = false, HelpText = "Print this help text and quit")]
+            public bool Help { get; set; } = false;
+
+            [Option('v', "version", Required = false, HelpText = "Print version number and quit")]
+            public bool Version { get; set; } = false;
+        }
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o => Arguments = o).WithNotParsed(o => Logger.Error("Unable to parse arguments: {0}", o.First().Tag));
+#if !DEBUG
+            if (Arguments.ConsoleEnabled)
+#else
+            ExternalConsole.InitConsole();
+#endif
+            var assembly = System.Reflection.Assembly.GetEntryAssembly().GetName();
+            Logger.Debug("{0} v{1} ({2}) with args: {3}", assembly.Name, assembly.Version, assembly.ProcessorArchitecture, string.Join(" ", args));
+            Logger.Debug("Current Date and Time: {0} (UTC: {1})", DateTime.Now, DateTime.UtcNow);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+            Logger.Debug("Ended");
+        }
+
+        private static void onParseError()
+        {
+        }
+    }
+}
---
+++ UI/Main.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Spicetify_GUI_Wrapper
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.btn_apply = new System.Windows.Forms.Button();
+            this.btn_backup = new System.Windows.Forms.Button();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.lst_extensions = new System.Windows.Forms.CheckedListBox();
+            this.txt_extension = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.lst_styles = new System.Windows.Forms.CheckedListBox();
+            this.txt_style = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Location = new System.Drawing.Point(12, 12);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(868, 500);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(860, 474);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Features";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.splitContainer1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(860, 474);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Extensions";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // btn_apply
+            // 
+            this.btn_apply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_apply.Location = new System.Drawing.Point(807, 518);
+            this.btn_apply.Name = "btn_apply";
+            this.btn_apply.Size = new System.Drawing.Size(73, 25);
+            this.btn_apply.TabIndex = 1;
+            this.btn_apply.Text = "Apply";
+            this.btn_apply.UseVisualStyleBackColor = true;
+            // 
+            // btn_backup
+            // 
+            this.btn_backup.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_backup.Location = new System.Drawing.Point(728, 518);
+            this.btn_backup.Name = "btn_backup";
+            this.btn_backup.Size = new System.Drawing.Size(73, 25);
+            this.btn_backup.TabIndex = 2;
+            this.btn_backup.Text = "Backup";
+            this.btn_backup.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.splitContainer2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(860, 474);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Styles";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(860, 474);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Config";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // lst_extensions
+            // 
+            this.lst_extensions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lst_extensions.FormattingEnabled = true;
+            this.lst_extensions.Location = new System.Drawing.Point(0, 0);
+            this.lst_extensions.Name = "lst_extensions";
+            this.lst_extensions.Size = new System.Drawing.Size(284, 468);
+            this.lst_extensions.TabIndex = 0;
+            // 
+            // txt_extension
+            // 
+            this.txt_extension.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_extension.Location = new System.Drawing.Point(0, 0);
+            this.txt_extension.Name = "txt_extension";
+            this.txt_extension.Size = new System.Drawing.Size(566, 468);
+            this.txt_extension.TabIndex = 1;
+            this.txt_extension.Text = "";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(3, 3);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.lst_extensions);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.txt_extension);
+            this.splitContainer1.Size = new System.Drawing.Size(854, 468);
+            this.splitContainer1.SplitterDistance = 284;
+            this.splitContainer1.TabIndex = 2;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.lst_styles);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.txt_style);
+            this.splitContainer2.Size = new System.Drawing.Size(860, 474);
+            this.splitContainer2.SplitterDistance = 285;
+            this.splitContainer2.TabIndex = 3;
+            // 
+            // lst_styles
+            // 
+            this.lst_styles.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lst_styles.FormattingEnabled = true;
+            this.lst_styles.Location = new System.Drawing.Point(0, 0);
+            this.lst_styles.Name = "lst_styles";
+            this.lst_styles.Size = new System.Drawing.Size(285, 474);
+            this.lst_styles.TabIndex = 0;
+            // 
+            // txt_style
+            // 
+            this.txt_style.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txt_style.Location = new System.Drawing.Point(0, 0);
+            this.txt_style.Name = "txt_style";
+            this.txt_style.Size = new System.Drawing.Size(571, 474);
+            this.txt_style.TabIndex = 1;
+            this.txt_style.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(892, 554);
+            this.Controls.Add(this.btn_backup);
+            this.Controls.Add(this.btn_apply);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Spicetify GUI Wrapper";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.CheckedListBox lst_extensions;
+        private System.Windows.Forms.RichTextBox txt_extension;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.CheckedListBox lst_styles;
+        private System.Windows.Forms.RichTextBox txt_style;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Button btn_apply;
+        private System.Windows.Forms.Button btn_backup;
+    }
+}
+
